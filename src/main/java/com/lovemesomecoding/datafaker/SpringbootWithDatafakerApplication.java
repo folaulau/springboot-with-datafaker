e01fd234b2708e9492a332ba98692789e6730a6e
folaulau <folaukaveinga@gmail.com>
2023-01-25 09:29:48 -0700

    setup

---
+++ src/main/java/com/lovemesomecoding/datafaker/SpringbootWithDatafakerApplication.java
@@ -0,0 +1,13 @@
+package com.lovemesomecoding.datafaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithDatafakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithDatafakerApplication.class, args);
+	}
+
+}
